552d131f92f96952f3a0f9098106341e6cffad76
Ing. Miroslav Jaščur <miroslav.jascur@tuke.sk>
2020-04-06 16:15:10 +0200

    new file:   landmark_node_glob_debug.launch.sh 	deleted:    rosbag.sh 	deleted:    ../ouster_slam/launch/os1_packet_record.launch 	modified:   ../packet_recorder/launch/os1_packet_record.launch 	modified:   ../packet_recorder/record_packets.sh

--- packet_recorder/record_packets.sh
+++ packet_recorder/record_packets.sh
@@ -4,7 +4,7 @@ source ../../devel/setup.bash
 BAGFILEPATH='/mnt/b6ef98c1-c7b0-4b69-87d0-2165d664c748/bags/geo/stvorec_proti_smeru_hr_2.bag' #own path
 METAFILEPATH='/mnt/b6ef98c1-c7b0-4b69-87d0-2165d664c748/bags/geo/config.json' #own path
 
-RECORDFILEPATH='/mnt/b6ef98c1-c7b0-4b69-87d0-2165d664c748/bags/debug/';
+RECORDFILEPATH='/mnt/b6ef98c1-c7b0-4b69-87d0-2165d664c748/bags/debug/dsaada/';
 
 roslaunch packet_recorder os1_packet_record.launch\
                              udp_hostname:=127.0.0.1\
